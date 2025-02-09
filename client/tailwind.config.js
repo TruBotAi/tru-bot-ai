f862577bda5089ed89625fe26f1fdbee2b89e326
utkarsh birla <utkarshbirla3@gmail.com>
2025-02-10 01:23:37 +0530

    [UB] - Issues resolved

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ensure correct file paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
