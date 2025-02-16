a44f0510f489952b15ace97ba4b30d534010c834
Elvis <Elvisraphael00@gmail.com>
2025-02-16 23:03:11 +0100

    `Added Hero component to App and modified Hero component styles and content`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "../src/assets/sass/App.scss"
 import Header from "./components/Header"
 import "../src/assets/sass/index.scss"
+import Hero from "./components/Hero"
 
 function App() {
   return (
     <>
       <main>
         <Header />
+        <Hero />
       </main>
     </>
   )
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,13 +1,13 @@
 function Hero() {
   return (
     <>
-      <section className='hero center'>
-        <div className='title'>
-          <h1 className='text-5xl font-bold'>The only chatbot platform you need</h1>
-          <p>The only easy-to-use chatbot platform you need to streamline lead gen, customer support and other use cases – at scale and cost-effectively.</p>
-          <div className='btn'>
-            <button className='p-2 rounded-xl bg-blue-400 text-white'>Show demo</button>
-            <button className='bg-transparent text-white border-1 border-indigo-500 '>Try for free</button>
+      <section className='hero text-center p-9'>
+        <div className='title text-white'>
+          <h1 className='text-7xl font-bold '>The only chatbot platform you need</h1>
+          <p className='text-lg text-white-50 my-4'>The only easy-to-use chatbot platform you need to streamline lead gen, customer support and other use cases – at scale and cost-effectively.</p>
+          <div className='btn w-fit m-auto flex items-center gap-4'>
+            <button className='p-5 rounded-xl bg-blue-400 text-white font-bold text-lg hover:bg-blue-600'>Show demo</button>
+            <button className='bg-transparent text-white border-1 border-indigo-500 p-5 font-bold rounded-xl text-lg'>Try for free</button>
           </div>
         </div>
       </section>
