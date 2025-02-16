2874239e1352bf8853cab4ad92a99fc1b295f0fe
Elvis <Elvisraphael00@gmail.com>
2025-02-16 22:51:11 +0100

    `Added title and call-to-action buttons to Hero component`

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,8 +2,14 @@ function Hero() {
   return (
     <>
       <section className='hero center'>
-        <h1 className='text-2xl '>The only chatbot platform you need</h1>
-        <p>The only easy-to-use chatbot platform you need to streamline lead gen, customer support and other use cases – at scale and cost-effectively.</p>
+        <div className='title'>
+          <h1 className='text-5xl font-bold'>The only chatbot platform you need</h1>
+          <p>The only easy-to-use chatbot platform you need to streamline lead gen, customer support and other use cases – at scale and cost-effectively.</p>
+          <div className='btn'>
+            <button className='p-2 rounded-xl bg-blue-400 text-white'>Show demo</button>
+            <button className='bg-transparent text-white border-1 border-indigo-500 '>Try for free</button>
+          </div>
+        </div>
       </section>
     </>
   )
