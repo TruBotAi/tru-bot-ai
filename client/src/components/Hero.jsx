d1dca76cc28a3a3729a1cc057dae8952b87d8358
Elvis <Elvisraphael00@gmail.com>
2025-02-16 22:47:06 +0100

    `Removed nav styles and added hero section content`

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,10 @@
 function Hero() {
   return (
     <>
-      <section className='hero'></section>
+      <section className='hero center'>
+        <h1 className='text-2xl '>The only chatbot platform you need</h1>
+        <p>The only easy-to-use chatbot platform you need to streamline lead gen, customer support and other use cases – at scale and cost-effectively.</p>
+      </section>
     </>
   )
 }
